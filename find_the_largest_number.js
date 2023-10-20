2d5a80debeec0e222f26105a74e5217f9f213f8f
Anh bui <buihoangducanh0987@gmail.com>
2023-10-20 23:13:46 +0700

    fix func name

--- find_the_largest_number.js
+++ find_the_largest_number.js
@@ -1,7 +1,7 @@
 // Find the Largest Number:
 // Write a function that returns the largest number in an array.
 
-function largetNum(arr) {
+function largestNum(arr) {
   if (arr.length === 0) return undefined;
 
   let max = Number.MIN_VALUE;
@@ -12,5 +12,5 @@ function largetNum(arr) {
 }
 
 //test
-console.log(largetNum([]));
-console.log(largetNum([-1, 1, 2, 3, 4, 15]));
+console.log(largestNum([]));
+console.log(largestNum([-1, 1, 2, 3, 4, 15]));
